847c49eae5408b39b0c0405e9e1a7da6055e8634
Your Name <you@example.com>
2024-02-17 15:40:59 +0200

    зробив class

--- main.py
+++ main.py
@@ -2,24 +2,37 @@
 
 import tkinter as tk
 
-window = tk.Tk()
-ROW = 5
-COLUMNS = 3
+class MineSweeper:
+    
+    window = tk.Tk()
+    ROW = 10
+    COLUMNS = 7
+    
+    def __init__(self):
+        self.buttons = []
+        for i in range(MineSweeper.ROW):
+            temp = []
+            for j in range(MineSweeper.COLUMNS):
+                btn = tk.Button(MineSweeper.window, width=3, font='Calibri 15 bold')
+                temp.append(btn)
+            self.buttons.append(temp)
+    
+    def create_widgets(self):
+        for i in range(MineSweeper.ROW):
+            for j in range(MineSweeper.COLUMNS):
+                btn = self.buttons[i][j]
+                btn.grid(row=i, column=j)    
+    
+    def start(self):
+        self.create_widgets()
+        self.print_buttons()
+        MineSweeper.window.mainloop()
+    
+    def print_buttons(self):
+        for row_btn in self.buttons:
+            print(row_btn)
 
-buttons = []
-for i in range(ROW):
-    temp = []
-    for j in range(COLUMNS):
-        btn = tk.Button(window)
-        temp.append(btn)
-    buttons.append(temp)
 
-for row_btn in buttons:
-    print(row_btn)
+game = MineSweeper() 
+game.start()
 
-
-
-
-
-
-window.mainloop()
